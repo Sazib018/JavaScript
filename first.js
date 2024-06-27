dc88e84e299dc3542d67b115769e5befd71a33f4
Sazib <mahiburislamsazib@gmail.com>
2024-06-27 14:24:05 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,26 @@
+//Variable of js
+
+//Number
+var price= 2000;
+var father_age=50;
+
+//string
+
+var friend="Mahfuj";
+var age="26";
+var address="chakpara,laxmikol,baraigram,natore";
+var name='asif';
+var bro=`rinku`;
+
+//Boolean
+
+var isMale= true;
+var isFemale= false;
+
+//use like this
+
+var myfriend='mahfuj';
+var my_friend='mahfuj'; //shanke case
+var myFriend='mahfuj'; //camel case
+
+var areYouGoToSchool=true; //camel case 
